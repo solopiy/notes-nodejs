a289c46692bb56771fba841db7956d1aef3eead6
Misha Solopiy <mixanstr@gmail.com>
2017-11-27 03:54:21 +0300

    Init project

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+
+module.exports = router;
